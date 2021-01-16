e912a4b00337a859fb59be069031ce1b7f7b046d
xperrylinn <xlinn@citrine.io>
2021-01-15 16:41:00 -0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='dagre_py',
+    version='1.0',
+    description='',
+    author='',
+    author_email='',
+    packages=['dagre_py'],  #same as name
+    install_requires=[],    #external packages as dependencies
+)
